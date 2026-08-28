c65824df2047ccfa39a3c7a568f029cfb47f233b
Noe Oswaldo Escobal Huayana <noe.escobal@tecsup.edu.pe>
2026-08-27 22:51:30 -0500

    Semana 02 - R2: Crea estructura inicial del proyecto POO

---
+++ Semana02/R2-ProyectoConIA/CarritoPOO/poo/src/main/java/com/escobal/poo/Main.kt
@@ -0,0 +1,8 @@
+package com.escobal.poo
+
+fun main() {
+    println("======================================")
+    println(" CARRITO DE COMPRAS - VERSION POO ")
+    println("======================================")
+    println("Proyecto R2 iniciado correctamente")
+}
