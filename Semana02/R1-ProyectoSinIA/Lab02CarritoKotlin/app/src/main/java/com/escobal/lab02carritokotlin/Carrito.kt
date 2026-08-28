5dd3272df815bac373c985d30323990348a73826
Noe Oswaldo Escobal Huayana <noe.escobal@tecsup.edu.pe>
2026-08-27 21:43:47 -0500

    Semana 02 - R1: Proyecto inicial con menu del carrito

---
+++ Semana02/R1-ProyectoSinIA/Lab02CarritoKotlin/app/src/main/java/com/escobal/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.escobal.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
---
+++ Semana02/R1-ProyectoSinIA/Lab02CarritoKotlin/carrito/src/main/java/com/escobal/carrito/Carrito.kt
@@ -0,0 +1,7 @@
+package com.escobal.carrito
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
