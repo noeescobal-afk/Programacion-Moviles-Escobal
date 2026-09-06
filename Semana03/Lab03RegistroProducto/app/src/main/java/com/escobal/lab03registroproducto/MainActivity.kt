b3c9b6471e55d4bc7078f2536508d6d772a057f8
Noe Oswaldo Escobal Huayana <noe.escobal@tecsup.edu.pe>
2026-09-05 20:15:52 -0500

    Semana 03 - C3: Agregar campos de registro de producto

--- Semana03/Lab03RegistroProducto/app/src/main/java/com/escobal/lab03registroproducto/MainActivity.kt
+++ Semana03/Lab03RegistroProducto/app/src/main/java/com/escobal/lab03registroproducto/MainActivity.kt
@@ -8,6 +8,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.material3.OutlinedTextField
 
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +29,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RegistroProductoScreen(){
+
+    var nombre by remember {
+        mutableStateOf("")
+    }
+
+    var precio by remember {
+        mutableStateOf("")
+    }
+
+    var cantidad by remember {
+        mutableStateOf("")
+    }
+
 
     Column(
         modifier = Modifier
@@ -44,6 +62,57 @@ fun RegistroProductoScreen(){
 
         Text(
             text = "Complete los datos del producto para registrarlo"
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = {
+                nombre = it
+            },
+            label = {
+                Text("Nombre del producto")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        OutlinedTextField(
+            value = precio,
+            onValueChange = {
+                precio = it
+            },
+            label = {
+                Text("Precio")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        OutlinedTextField(
+            value = cantidad,
+            onValueChange = {
+                cantidad = it
+            },
+            label = {
+                Text("Cantidad")
+            },
+            modifier = Modifier.fillMaxWidth()
         )
 
     }
