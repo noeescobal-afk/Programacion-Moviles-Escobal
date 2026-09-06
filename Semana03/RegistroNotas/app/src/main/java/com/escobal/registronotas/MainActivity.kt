cd644b994a348c06f05f3706d3a514658a7836cf
Noe Oswaldo Escobal Huayana <noe.escobal@tecsup.edu.pe>
2026-09-05 20:48:21 -0500

    Semana 03 - T2: Agregar estructura y estados de cursos

--- Semana03/RegistroNotas/app/src/main/java/com/escobal/registronotas/MainActivity.kt
+++ Semana03/RegistroNotas/app/src/main/java/com/escobal/registronotas/MainActivity.kt
@@ -3,45 +3,132 @@ package com.escobal.registronotas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.escobal.registronotas.ui.theme.RegistroNotasTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            RegistroNotasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            RegistroNotasScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RegistroNotasTheme {
-        Greeting("Android")
+fun RegistroNotasScreen() {
+
+
+    var notaFundamentos by remember {
+        mutableStateOf(0)
     }
+
+
+    var notaPOO by remember {
+        mutableStateOf(0)
+    }
+
+
+    var notaMoviles by remember {
+        mutableStateOf(0)
+    }
+
+
+    var notaBaseDatos by remember {
+        mutableStateOf(0)
+    }
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+
+
+        Text(
+            text = "Registro de Notas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        Text(
+            text = "Calcula tu promedio ponderado del ciclo"
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+
+        Text(
+            text = "Fundamentos de Programación (20%)"
+        )
+
+
+        Text(
+            text = "Nota actual: $notaFundamentos"
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        Text(
+            text = "Programación Orientada a Objetos (25%)"
+        )
+
+
+        Text(
+            text = "Nota actual: $notaPOO"
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        Text(
+            text = "Programación en Móviles (30%)"
+        )
+
+
+        Text(
+            text = "Nota actual: $notaMoviles"
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(12.dp)
+        )
+
+
+        Text(
+            text = "Base de Datos (25%)"
+        )
+
+
+        Text(
+            text = "Nota actual: $notaBaseDatos"
+        )
+
+    }
+
 }
